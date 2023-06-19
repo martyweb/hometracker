d25fcfb3380dfdcbf3554aedd27d007f4704900c
Marty <marty@martyweb.com>
2023-06-19 16:18:32 -0500

    moved config to file

--- utils/plugins.py
+++ utils/plugins.py
@@ -6,10 +6,17 @@ class plugins:
     Plugin utils
     '''
     def get_values():
+        
+        import json
 
-        return {
-            "air_quality": {"ids": "22553", "key": "7B5CF0D1-ECD4-11EC-8561-42010A800005", "interval": 60},
-            "weather": {"appid": "15c6d1063b5b819c4a1eb6e47dc8dd4b", "zip": "60564", "interval": 60},
-            "speed_test": {"zip": "60564"},
-            "pollution": {"zips": "60564", "key": "15c6d1063b5b819c4a1eb6e47dc8dd4b", "interval": 60},
-        }
+        with open('config.json', 'r') as f:
+            data = json.load(f)
+
+        return data
+
+        # return {
+        #     "air_quality": {"ids": "22553", "key": "7B5CF0D1-ECD4-11EC-8561-42010A800005", "interval": 60},
+        #     "weather": {"appid": "15c6d1063b5b819c4a1eb6e47dc8dd4b", "zip": "60564", "interval": 60},
+        #     "speed_test": {"zip": "60564", "interval": 60},
+        #     "pollution": {"zips": "60564", "key": "15c6d1063b5b819c4a1eb6e47dc8dd4b", "interval": 60},
+        # }
