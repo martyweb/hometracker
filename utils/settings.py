17fdda829bb1e1a20f60a53adf3e1369a6a8c343
Marty <marty@martyweb.com>
2023-06-19 16:34:00 -0500

    override with env vars

--- utils/settings.py
+++ utils/settings.py
@@ -1,12 +1,18 @@
-# import json
+import json
+import os
 
 class settings:
     '''
     Application Settings
     '''
-
     def get_values():
         with open('config-app.json', 'r') as f:
             data = json.load(f)
+
+        #override with env vars
+        for setting in data:
+            if os.getenv(setting) is not None: data[setting]=os.getenv(setting)
+
+        #TODO: check data from file
 
         return data
