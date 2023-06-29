61d6e96c0809e8a03371f4154c041c0f8f5eceb2
Marty <marty@martyweb.com>
2023-06-29 02:20:26 -0500

    adjusted default setting

--- utils/settings.py
+++ utils/settings.py
@@ -6,13 +6,29 @@ class settings:
     Application Settings
     '''
     def get_values():
+
         with open('config-app.json', 'r') as f:
             data = json.load(f)
 
         #override with env vars
         for setting in data:
             if os.getenv(setting) is not None: data[setting]=os.getenv(setting)
+        for setting in settings.available_settings():
+            if os.getenv(setting) is not None: data[setting]=os.getenv(setting)
 
         #TODO: check data from file
 
         return data
+    
+    def available_settings():
+        return {
+            "influxdbhost": "localhost",
+            "influxdbport": "8086",
+            "influxdbusername": "",
+            "influxdbpass": "",
+            "influxdbdatabase":"HomeStatus",
+            "db_file": "database.db",
+            "plugin_path": "plugins",
+            "db_path": "db",
+            "granfana_host": ""
+        }
