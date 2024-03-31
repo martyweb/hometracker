9f598fad9ce6a78d1b4558031322d82af5523aa8
Marty <marty@martyweb.com>
2024-03-31 14:12:10 -0500

    openweathermap zip to lat/lon

--- plugins/pollution.py
+++ plugins/pollution.py
@@ -18,18 +18,22 @@ class pollution:
         for zip in zips:
             print("Getting zipcode " + zip)
 
-            url = 'https://nominatim.openstreetmap.org/search?q=' + \
-                urllib.parse.quote(zip) + '&format=json'
+            url = 'http://api.openweathermap.org/geo/1.0/zip?zip=' + \
+                urllib.parse.quote(zip) + ',' + \
+                'US' + '&appid=' + values["key"]
+                        
             address_data = requests.get(url)
-            # print(address_data.status_code)
+            #print(address_data.json())
             if(address_data.status_code != 200):
                 raise Exception("Bad return status " +
                                 str(address_data.status_code))
             address_data = address_data.json()
 
+            print("Getting pollution for " + str(address_data["lat"]) + " " + str(address_data["lon"]))
+
             url = 'http://api.openweathermap.org/data/2.5/air_pollution?lat=' + \
-                address_data[0]["lat"] + '&lon=' + \
-                address_data[0]["lon"] + '&appid=' + values["key"]
+                str(address_data["lat"]) + '&lon=' + \
+                str(address_data["lon"]) + '&appid=' + values["key"]
             json_data = requests.get(url).json()
 
             # print("Got this data back:")
